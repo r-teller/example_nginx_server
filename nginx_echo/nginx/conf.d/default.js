d759d5b08cd20ab109ca77b6918503dc8935d83f
rotelly <31879487+rotelly@users.noreply.github.com>
2020-01-28 12:23:37 -0800

    nginx_echo migrated to NJS

--- nginx_echo/nginx/conf.d/default.js
+++ nginx_echo/nginx/conf.d/default.js
@@ -28,7 +28,7 @@ function echo(r) {
     _responseBody.request.network.serverAddress = r.variables.server_addr;
     _responseBody.request.network.serverPort = r.variables.server_port;
 
-    _responseBody.request.ssl.isHttps = r.variables.isHttps;
+    _responseBody.request.ssl.isHttps = r.variables.Https != 'undefined' && r.variables == 'on' ? true : false;
     _responseBody.request.ssl.sslProtocol = r.variables.isHttps != 'undefined' ? r.variables.ssl_protocol : null;
     _responseBody.request.ssl.sslCipher = r.variables.isHttps != 'undefined' ? r.variables.ssl_cipher : null;
 
@@ -37,9 +37,61 @@ function echo(r) {
     _responseBody.request.session.connection = r.variables.connection;
     _responseBody.request.session.connectionNumber = r.variables.connection_requests;
     
+    // Default response values
     _responseBody.response.statusCode = 200;
-    _responseBody.response.statusReason = 'testing';
+    _responseBody.response.statusReason = 'DEFAULT';
     _responseBody.response.statusBody = 'HEALTHY';
+
+    switch (true) {
+        case (r.variables.server_port >= 5000 && r.variables.server_port < 6000):
+            // Captures requests recieved on TCP port 5000-5999
+            _responseBody.response.statusCode = 500;
+            _responseBody.response.statusReason = 'UNHEALTHY_SERVER_PORT_RANGE';
+            _responseBody.response.statusBody = 'UNHEALTHY';
+            break;
+        case (r.variables.server_port >= 6000 && r.variables.server_port < 7000):
+            // Captures requests recieved on TCP port 6000-6999
+            _responseBody.response.statusReason = 'RANDOM_SERVER_PORT_RANGE';
+            if (Math.floor(Math.random() * 2)) {
+                _responseBody.response.statusCode = 200;
+                _responseBody.response.statusBody = 'HEALTHY';
+            } else {
+                _responseBody.response.statusCode = 500;
+                _responseBody.response.statusBody = 'UNHEALTHY';
+            }
+
+            break;
+        case (r.args.status != undefined && typeof(r.args.status) == number):
+            // Captures requests recieved on TCP port 6000-6999
+            _responseBody.response.statusCode = r.args.status;
+            switch (true) {
+                case (r.args.status >= 200 && && r.args.status < 299):
+                    _responseBody.response.statusReason = 'QUERY_PARAM';
+                    _responseBody.response.statusBody = 'HEALTHY';
+                    break;
+                case (r.args.status >= 300 && && r.args.status < 399):
+                    _responseBody.response.statusReason = 'QUERY_PARAM';
+                    _responseBody.response.statusBody = 'REDIRECT';
+                    break;
+                case (r.args.status >= 400 && && r.args.status < 499):
+                    _responseBody.response.statusReason = 'QUERY_PARAM';
+                    _responseBody.response.statusBody = 'UNAUTHORIZED';
+                    break;
+                case (r.args.status >= 500 && && r.args.status < 599):
+                    _responseBody.response.statusReason = 'QUERY_PARAM';
+                    _responseBody.response.statusBody = 'UNHEALTHY';
+                    break;
+                default:
+                    _responseBody.response.statusReason = 'QUERY_PARAM';
+                    _responseBody.response.statusBody = 'UNKNOWN_QUERY_PARAM';
+                    break;
+            }
+            break;
+        default:
+            // Captures everything else
+            break;
+    }
+    
     _responseBody.response.timeStamp = r.variables.time_iso8601;
 
     r.return(_responseBody.response.statusCode, JSON.stringify(_responseBody) + '\n');
