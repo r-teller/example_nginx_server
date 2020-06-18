5a55d900c9a062d701230190ece7ea764b533f55
Robert Teller <r.teller@f5.com>
2020-06-18 09:43:20 -0700

    adding flush param for app_protect

--- nginx_app_protect_stats/ReadMe.md
+++ nginx_app_protect_stats/ReadMe.md
@@ -4,21 +4,39 @@ The nginx.conf uses the keyval store to track incoming requests and NJS to incrm
 
 This is not meant to be a long lived solution but a POC
 
+App Protect access_log docuemtnation can be found --> https://docs.nginx.com/nginx-app-protect/troubleshooting/#access-logs
+
+*Note: to flush app_protect stats use query param flush=true, to flush for a specific policy name use query param policy_name=<policy-name>
+
 ```bash
 root@curl 127.0.0.1/api/6/http/app_protect  -s  | jq
 {
   "app_protect_default_policy": {
-    "2.52.5": {
-      "OUTCOME": {
-        "PASSED": 7,
-        "REJECTED": 4
-      },
-      "REASON": {
-        "SECURITY_WAF_OK": 0,
-        "SECURITY_WAF_VIOLATION": 1,
-        "SECURITY_WAF_FLAGGED": 3,
-        "SECURITY_WAF_BYPASS": 0
-      }
+    "OUTCOME": {
+      "PASSED": 13,
+      "REJECTED": 17
+    },
+    "REASON": {
+      "SECURITY_WAF_OK": 0,
+      "SECURITY_WAF_VIOLATION": 17,
+      "SECURITY_WAF_FLAGGED": 13,
+      "SECURITY_WAF_BYPASS": 0
+    }
+  }
+}
+
+root@curl "127.0.0.1/api/6/http/app_protect?flush=true&policy_name=app_protect_default_policy"  -s  | jq
+{
+  "app_protect_default_policy": {
+    "OUTCOME": {
+      "PASSED": 0,
+      "REJECTED": 0
+    },
+    "REASON": {
+      "SECURITY_WAF_OK": 0,
+      "SECURITY_WAF_VIOLATION": 0,
+      "SECURITY_WAF_FLAGGED": 0,
+      "SECURITY_WAF_BYPASS": 0
     }
   }
 }
--- nginx_app_protect_stats/http.js
+++ nginx_app_protect_stats/http.js
@@ -7,33 +7,59 @@ function app_protect_stats(r) {
     }
 
      if (app_protect_stats[r.variables.app_protect_policy_name] === undefined ){
-        app_protect_stats[r.variables.app_protect_policy_name] = {};
+        app_protect_stats[r.variables.app_protect_policy_name] = {
+            "OUTCOME":{
+                "PASSED": 0,
+                "REJECTED": 0
+             },
+            "REASON":{
+                "SECURITY_WAF_OK": 0,
+                "SECURITY_WAF_VIOLATION": 0,
+                "SECURITY_WAF_FLAGGED": 0,
+                "SECURITY_WAF_BYPASS": 0
+            }
+        }
     }
 
-    if (app_protect_stats[r.variables.app_protect_policy_name][r.variables.app_protect_version] === undefined ){
-       app_protect_stats[r.variables.app_protect_policy_name][r.variables.app_protect_version] = {
-           "OUTCOME":{
-               "PASSED": 0,
-               "REJECTED": 0
-            },
-           "REASON":{
-               "SECURITY_WAF_OK": 0,
-               "SECURITY_WAF_VIOLATION": 0,
-               "SECURITY_WAF_FLAGGED": 0,
-               "SECURITY_WAF_BYPASS": 0
-           }
-       };
-   }
-
-   app_protect_stats[r.variables.app_protect_policy_name][r.variables.app_protect_version].OUTCOME[r.variables.app_protect_outcome] += 1;
-   app_protect_stats[r.variables.app_protect_policy_name][r.variables.app_protect_version].REASON[r.variables.app_protect_outcome_reason] += 1;
+   app_protect_stats[r.variables.app_protect_policy_name].OUTCOME[r.variables.app_protect_outcome] += 1;
+   app_protect_stats[r.variables.app_protect_policy_name].REASON[r.variables.app_protect_outcome_reason] += 1;
 
     r.variables.ap_data = JSON.stringify(app_protect_stats);
 
     return '200'
 }
 
+function flush_stats(r){
+    if (r.args.policy_name != undefined) {
+        app_protect_stats = JSON.parse(r.variables.ap_data);
+        app_protect_stats[r.args.policy_name] = {
+            "OUTCOME":{
+                "PASSED": 0,
+                "REJECTED": 0
+             },
+            "REASON":{
+                "SECURITY_WAF_OK": 0,
+                "SECURITY_WAF_VIOLATION": 0,
+                "SECURITY_WAF_FLAGGED": 0,
+                "SECURITY_WAF_BYPASS": 0
+            }
+        };
+        r.variables.ap_data = JSON.stringify(app_protect_stats);
+    } else {
+        r.variables.ap_data =  JSON.stringify({});
+    }
+    return 'test';
+}
+
 function app_protect_stats_json(r){
+
+    switch (true) {
+        case (r.args.flush != undefined && JSON.parse(r.args.flush.toLowerCase())):
+            flush_stats(r);
+            break;
+        default:
+            break;
+    }
     if (r.variables.ap_data === undefined ) {
         var app_protect_stats = {};
     } else {
